bdbfde3d801df2c996eaa83fc822b4d48fd8737d
siric156 <siric156@newschool.edu>
2025-12-13 19:49:50 -0500

    Refactor main.py to disable unused code
    Comment out unused imports and model initialization.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # pip install streamlit
 import streamlit as st
-from torchvision.models import resnet50, ResNet50_Weights
-from torchvision.io import decode_image
+#from torchvision.models import resnet50, ResNet50_Weights
+from ultralytics import YOLO
+#from torchvision.io import decode_image
 from PIL import Image
-from torchvision.transforms import ToTensor
-import torch
+#from torchvision.transforms import ToTensor
+#import torch
 import io
-import streamlit as st
 from gtts import gTTS
 
 def text_to_speech_bytes(text: str, lang: str = "en") -> bytes:
@@ -16,9 +16,9 @@ def text_to_speech_bytes(text: str, lang: str = "en") -> bytes:
     return fp.read()
 
 # bring in model and all
-weights = ResNet50_Weights.DEFAULT
-transforms = weights.transforms()
-model = resnet50(weights=weights)
+#weights = ResNet50_Weights.DEFAULT
+#transforms = weights.transforms()
+#model = resnet50(weights=weights)
 
 st.header('A tool for classifying images for Visual Aids')
 
@@ -28,15 +28,17 @@ image = st.camera_input("Take a picture")
 if image is not None:   
     st.image(image)
     image = Image.open(image)
-    t = ToTensor()
-    image = t(image)
+    results = model.predict(source=image, save=False)  # save plotted images
+    #t = ToTensor()
+    #image = t(image)
 
-    transformed_image = transforms(image[:, :, :])
+    #transformed_image = transforms(image[:, :, :])
 
 #pass it through the model
-    prediction = model(transformed_image.unsqueeze(0))
-    pred_val = torch.argmax(prediction)
+    #prediction = model(transformed_image.unsqueeze(0))
+    #pred_val = torch.argmax(prediction)
 #st.write(pred_val)
-    st.write(weights.meta['categories'][pred_val])
+    #st.write(weights.meta['categories'][pred_val])
+    st.write(results)
     audio = text_to_speech_bytes(weights.meta['categories'][pred_val])
     st.audio(audio, format="audio/mp3", autoplay=True)
