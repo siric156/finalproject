5f9127087e10c42859cc7009f28b2df9d4081efb
siric156 <siric156@newschool.edu>
2025-12-14 14:29:30 -0500

    Refactor main.py to streamline imports and add captioning
    Removed unused imports and commented code. Added BlipProcessor and BlipForConditionalGeneration for image captioning.

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
-# pip install streamlit
 import streamlit as st
-#from torchvision.models import resnet50, ResNet50_Weights
 from ultralytics import YOLO
-#from torchvision.io import decode_image
 from PIL import Image
-#from torchvision.transforms import ToTensor
-#import torch
 import io
 from gtts import gTTS
+from transformers import BlipProcessor, BlipForConditionalGeneration
 
 def text_to_speech_bytes(text: str, lang: str = "en") -> bytes:
     fp = io.BytesIO()
     gTTS(text=text, lang=lang).write_to_fp(fp)
     fp.seek(0)
     return fp.read()
-
-# bring in model and all
-#weights = ResNet50_Weights.DEFAULT
-#transforms = weights.transforms()
-#model = resnet50(weights=weights)
 
 st.header('A tool for classifying images for Visual Aids')
 
@@ -32,12 +23,6 @@ model = load()
 image = st.camera_input("Take a picture")
 if image is not None:   
     image = Image.open(image)
-    text = ""
-    inputs = processor(raw_image, text, return_tensors="pt")
-
-    out = model.generate(**inputs)
-    print(processor.decode(out[0], skip_special_tokens=True))
-    # >>> a photography of a woman and her dog
 
     # unconditional image captioning
     inputs = processor(raw_image, return_tensors="pt")
