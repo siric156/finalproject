4300b5ff86049ec9cc987141a0626c1c8d856d87
siric156 <siric156@newschool.edu>
2025-12-14 14:27:04 -0500

    Refactor image classification to use Blip model
    Refactor image processing and captioning logic using BlipProcessor and BlipForConditionalGeneration. Removed YOLO model references and updated input handling.

--- main.py
+++ main.py
@@ -23,24 +23,28 @@ def text_to_speech_bytes(text: str, lang: str = "en") -> bytes:
 st.header('A tool for classifying images for Visual Aids')
 
 st.write('Introduction to Python - Final Project')
-@st.cache_resource 
-def load():return YOLO("yolo11n.pt")
+@st.cache_resource
+def get_processor(): return BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+@st.cache_resource
+def load():return BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+processor = get_processor()
 model = load()
 image = st.camera_input("Take a picture")
 if image is not None:   
     image = Image.open(image)
-    results = model.predict(source=image, save=False)  # save plotted images
-    #t = ToTensor()
-    annotated = results[0].plot()
-    #image = t(image)
-    st.image(annotated)
-    #transformed_image = transforms(image[:, :, :])
+    text = ""
+    inputs = processor(raw_image, text, return_tensors="pt")
 
-#pass it through the model
-    #prediction = model(transformed_image.unsqueeze(0))
-    #pred_val = torch.argmax(prediction)
-#st.write(pred_val)
-    #st.write(weights.meta['categories'][pred_val])
-    st.write(results)
-    #audio = text_to_speech_bytes(weights.meta['categories'][pred_val])
-    #st.audio(audio, format="audio/mp3", autoplay=True)
+    out = model.generate(**inputs)
+    print(processor.decode(out[0], skip_special_tokens=True))
+    # >>> a photography of a woman and her dog
+
+    # unconditional image captioning
+    inputs = processor(raw_image, return_tensors="pt")
+
+    out = model.generate(**inputs)
+    text_out = (processor.decode(out[0], skip_special_tokens=True))
+    st.write (text_out)
+
+    audio = text_to_speech_bytes(text_out)
+    st.audio(audio, format="audio/mp3", autoplay=True)
